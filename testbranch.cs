b995043497e169ac6df18d19270c4a0e51a082fe
adhiyan <adhiyanabdiel@gmail.com>
2022-05-30 14:08:15 +0700

    membuat file branch

---
+++ testbranch.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text.Json;
+namespace demo.Models
+{
+    public class EmpClass
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public detailaddress address { get; set; }
+        public string phone { get; set; }
+        public string website { get; set; }
+        public detailcompany company { get; set; }
+    }
+
+    public class detailaddress
+    {
+        public string street { get; set; }
+        public string suite { get; set; }
+        public string city { get; set; }
+        public string zipcode { get; set; }
+        public coradress geo { get; set; }
+    }
+
+    public class coradress
+    {
+        public string lat { get; set; }
+        public string lng { get; set; }
+    }
+
+    public class detailcompany
+    {
+        public string name { get; set; }
+        public string catchPhrase { get; set; }
+        public string bs { get; set; }
+    }
+
+    public class displayinfo
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string website { get; set; }
+        public string address_street { get; set; }
+    }
+}
